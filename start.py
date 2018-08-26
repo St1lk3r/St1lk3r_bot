bf5565461cb975df21768c538f4969b80c9509c0
St1lk3r <42693766+St1lk3r@users.noreply.github.com>
2018-08-26 19:15:44 +0300

    Add files via upload

---
+++ start.py
@@ -0,0 +1,12 @@
+num1 = float(input("Enter 1st number "))
+num2 = float(input("Enter 2nd number "))
+num3 = input("Enter ")
+if num3 == '+':
+    print(num1+num2)
+elif num3 == '-':
+    print(num1-num2)
+elif num3 == '*':
+    print(num1*num2)
+elif num3 == '/':
+    print(num1/num2)
+           
